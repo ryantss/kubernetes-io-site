db7930c5b7fa5ffa7308205001465dde67a5406a
skyfish <skyfish81@gmail.com>
2018-02-11 18:27:06 +0800

    Simulate update to application

--- hellonode/server.js
+++ hellonode/server.js
@@ -3,7 +3,7 @@ var http = require('http');
 var handleRequest = function(request, response){
     console.log('Received request for URL: ' + request.url);
     response.writeHead(200);
-    response.end('Hellow World from Minikube tutorial!');
+    response.end('Hellow World from Minikube tutorial (AGAIN, to simulate updated to the app)!');
 };
 
 var www = http.createServer(handleRequest);
